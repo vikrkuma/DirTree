55475f8ef72b4a9d9e82a56cf66f539bbc0150ee
Vikrant Kumar <vikrant.kumar@toptal.com>
2022-10-27 17:05:09 +0530

    fix: mawell disk js sol

--- src/components/MaxWellDisk/MaxWellDisk.tsx
+++ src/components/MaxWellDisk/MaxWellDisk.tsx
@@ -1,7 +1,6 @@
+import { memo } from "react";
 import { RotationMethod } from "hooks/useRotationMethod";
-import { memo, useEffect, useRef } from "react";
-import styles from "./MaxWellDisk.module.scss";
-import { drawMaxwellDisc, animateDisk, stopAnimationOfDisk } from "./util";
+import JsCssMaxWellDisk from "components/JsCssMaxwellDisk";
 
 interface IMaxWellDiskProps {
   rotationMethod: RotationMethod;
@@ -9,31 +8,7 @@ interface IMaxWellDiskProps {
 }
 
 function MaxWellDisk({ rotationMethod, speedInMs }: IMaxWellDiskProps) {
-  const canvasRef = useRef<[HTMLCanvasElement, CanvasRenderingContext2D] | null>(null);
-  const cssAnimationSpeed = `${rotationMethod === RotationMethod.CSS ? speedInMs : 0}ms`;
-  const jsAnimationSpeed = rotationMethod === RotationMethod.JS ? speedInMs : 0;
-
-  useEffect(() => {
-    const canvas = document.getElementById("disc-canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    drawMaxwellDisc(canvas, ctx);
-    canvasRef.current = [canvas, ctx];
-
-    return stopAnimationOfDisk;
-  }, []);
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const [canvas, ctx] = canvasRef.current;
-      animateDisk(canvas, ctx, jsAnimationSpeed);
-    }
-  }, [canvasRef, jsAnimationSpeed]);
-
-  return (
-    <div className={styles.rotator} style={{ animationDuration: cssAnimationSpeed }}>
-      <canvas id="disc-canvas" width="500" height="500" />
-    </div>
-  );
+  return <JsCssMaxWellDisk rotationMethod={rotationMethod} speedInMs={speedInMs} />;
 }
 
 export default memo(MaxWellDisk);
