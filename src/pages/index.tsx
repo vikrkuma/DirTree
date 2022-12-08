e34f455dd60b29a49d2e960e691c22626634af68
Vikrant Kumar <vikrant.kumar@toptal.com>
2022-12-08 14:28:39 +0530

    feat: add next app bootstrap code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,10 @@
 import type { NextPage } from "next";
 import styles from "styles/Home.module.scss";
 
-import useRotationMethod from "hooks/useRotationMethod";
-import MaxWellDisk from "components/MaxWellDisk";
-import DiskRotationOptions from "components/DiskRotationOptions";
-
 const Home: NextPage = () => {
-  const [rotationMethod, speedInMs, setRotationMethod, setSpeedInMs] = useRotationMethod();
   return (
     <div className={styles.container}>
-      <MaxWellDisk rotationMethod={rotationMethod} speedInMs={speedInMs} />
-      <DiskRotationOptions
-        rotationMethod={rotationMethod}
-        speedInMs={speedInMs}
-        onRotationMethodChange={setRotationMethod}
-        onSpeedChange={setSpeedInMs}
-      />
+      <h1>Hello World!</h1>
     </div>
   );
 };
